9c1224159fe195e2a49ebc1b301d29d9ccd3ed06
kaikat0211 <kaifat7887@gmail.com>
2024-02-20 13:25:06 +0800

    GitHub OAuth success signin

--- app/page.tsx
+++ app/page.tsx
@@ -1,5 +1,9 @@
+import Link from "next/link";
 export default function Home() {
   return (
-    <main></main>
+    <>
+      <h1>Home</h1>
+      {/* <Link href='/api/auth/signin'>route</Link> */}
+    </>
   );
 }
--- next.config.mjs
+++ next.config.mjs
@@ -1,4 +1,11 @@
 /** @type {import('next').NextConfig} */
-const nextConfig = {};
-
-export default nextConfig;
+const nextConfig = {
+    env: {
+      GITHUB_CLIENT_ID: process.env.GITHUB_ID,
+      GITHUB_CLIENT_SECRET: process.env.GITHUB_SECRET,
+      NEXTAUTH_SECRET: process.env.NEXTAUTH_SECRET
+    },
+  };
+  
+  export default nextConfig;
+  
