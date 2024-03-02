f3810b368ab40e0b8a53d3cfa3cb7450bf057823
kaikat0211 <kaifat7887@gmail.com>
2024-03-02 21:33:41 +0800

    Add choose repo component

---
+++ app/components/ChooseRepo.tsx
@@ -0,0 +1,15 @@
+'use client'
+import { useAppSelector } from '@/lib/hooks'
+import React from 'react'
+const ChooseRepo = () => {
+    
+  return (
+    <>
+        <div className="w-full mt-20 flex justify-center">
+            <button className="border border-white bg-white text-red-200">123</button>
+        </div>
+    </>
+  )
+}
+
+export default ChooseRepo
